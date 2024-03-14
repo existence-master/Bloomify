e8d334f852bf0ea9d2f73338d4d7d733ee70a409
SK <sarthakkarandikar03@gmail.com>
2024-03-14 20:00:36 +0530

    reorganized folders

--- interface/tailwind.config.ts
+++ interface/tailwind.config.ts
@@ -7,7 +7,28 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      grey: "#949494",
+      flax: "#E9D985",
+      celadon: "#BCD8C1",
+      zomp: "#439A86",
+      cerulean: "#007991",
+      "midnight-azure": "#222E50",
+    },
     extend: {
+      fontFamily: {
+        opensans: ["OpenSans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        quicksand: ["Quicksand", "sans-serif"],
+      },
+      screens: {
+        xs: "128px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
